00e649b7107c8beb06f08147cd7ec4dc9a4714c0
Anna Zeit <anna.zeit@outlook.com>
2021-01-17 20:45:26 +0000

    My first Unity project

---
+++ Assets/SimpleRotation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour
+{
+    const float StepPerSecond = 200.0f;
+    
+    private Vector3 RotationPerSecond = new Vector3(0,0,10);
+
+    [Header("Speed Up Key")]
+    public KeyCode SpeedUp = KeyCode.None;
+
+    [Header("Speed Down Key")]
+    public KeyCode SpeedDown = KeyCode.None;
+
+    //public KeyCo
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        transform.Rotate(RotationPerSecond * Time.deltaTime);
+
+        if (Input.GetKey(SpeedUp)){
+            RotationPerSecond = new Vector3(
+                RotationPerSecond.x, 
+                RotationPerSecond.y, 
+                RotationPerSecond.z + (StepPerSecond * Time.deltaTime));
+        }
+         if (Input.GetKey(SpeedDown)){
+            RotationPerSecond = new Vector3(
+                RotationPerSecond.x,
+                RotationPerSecond.y,
+                RotationPerSecond.z - (StepPerSecond * Time.deltaTime));
+        }
+
+    }
+    
+}
+    
